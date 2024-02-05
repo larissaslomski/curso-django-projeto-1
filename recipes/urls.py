2feedee7318495be77cf4fa2a7fb8af122277bf8
larissa <larissa.slomsky@gmail.com>
2024-02-05 18:52:07 -0300

    adição de fontawesome no projeto

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from recipes.views import home, contact, about
-
+from recipes.views import home
 
 urlpatterns = [
-    path('sobre/', about),
-    path('', home),
-    path('contato/', contact),
+    path('', home)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'Larissa',
+        'name': 'Larissa'
     })
-
-
-def contact(request):
-    return HttpResponse('contato')
-
-
-def about(request):
-    return HttpResponse('sobre')
